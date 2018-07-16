f5a61a425a95b95d26779a4afefb59469e0e5ed7
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-16 13:59:17 +0200

    Első exception

---
+++ Exception/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog
+{
+    class Person
+    {
+        private int _salary;
+
+        public int Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value > 0) //TODO bejelzés
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+    }
+}
--- Exception/Program.cs
+++ Exception/Program.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exception
+namespace Prog
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                double result = Divide(13.4, 0);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException e)
+            {
+                //Console.WriteLine(e.StackTrace);
+                //Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
+            }
+            catch (InvalidOperationException ix)
+            {
+                Console.WriteLine("Elkaptam");
+            }
+
             Console.ReadKey();
+        }
+
+        static double Divide(double a, int b)
+        {
+            if (b == 0)
+                throw new InvalidOperationException("Nem lehet 0-val osztani");
+
+            return a / b;
         }
     }
 }
